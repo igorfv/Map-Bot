88372a711e4d691320a7fdebf87d90132f6ca3db
Igor Frias Vieira <igor.soad@gmail.com>
2013-09-13 00:41:40 -0300

    Anda para frente até encontra um objeto, acha o melhor caminho e continua andando

--- Map_Bot.ino
+++ Map_Bot.ino
@@ -11,8 +11,12 @@ long bt = 0; //Ultimo botao precionado
 Servo headServo;
 Servo neckServo;
 
-#define headServoCenter 12
-#define neckServoCenter 90
+#define headServoCenter 0
+#define neckServoCenter 86
+#define neckServoLeft 116
+#define neckServoRight 56
+
+#define mindist 20
 
 
 #define TRIGGER_PIN 12
@@ -20,13 +24,18 @@ Servo neckServo;
 
 Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
 
+boolean goon = true;
+boolean doublechecked = true;
+
+char lookto = 'c';
+char lookingto = 'c';
+int checkdist = 0;
+char shouldgo = 'c';
 
 void setup()
 {
   pinMode(M1, OUTPUT);
   pinMode(M2, OUTPUT);
-  
-  //Serial.begin(9600);
   
   headServo.attach(10);
   neckServo.attach(9);
@@ -36,17 +45,84 @@ void setup()
 }
 
 void loop()
-{ 
-//  if (Serial.available() > 0) {
-//    
-//    int valor = Serial.parseInt();
-//    Serial.println(valor, DEC);
-//  }
+{
   
   long microsec = ultrasonic.timing();
   float cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
   
-  andarFrente(0,0);
+  if(!goon)
+  {
+    switch (lookingto) {
+      case 'l':
+        //Left distance
+        checkdist = cmMsec;
+        shouldgo = 'l';
+        break;
+      case 'r':
+        //Right Distance
+        if(cmMsec > checkdist)
+        {
+          checkdist = cmMsec;
+          shouldgo = 'r';
+        }
+        break;
+      default: 
+        // Center distance (double checks)
+        if(cmMsec > checkdist)
+        {
+          checkdist = cmMsec;
+          shouldgo = 'c';
+        }
+        
+        if(checkdist < mindist)
+        {
+          shouldgo = 'b';
+        }
+        
+        thebestway();
+        
+        break;
+    }
+  }
+  
+  
+  if(cmMsec < mindist && goon)
+  {
+    goon = false;
+    doublechecked = false;
+    lookto = 'l';
+  }
+  
+  
+  if(goon)
+  {
+    andarFrente(0,0);
+  }
+  else
+  {    
+    parar(0,0);
+
+    switch (lookto) {
+      case 'l':
+        //Look left and check
+        neckServo.write(neckServoLeft);
+        lookto = 'r';
+        lookingto = 'l';
+        break;
+      case 'r':
+        //look right
+        neckServo.write(neckServoRight);
+        lookto = 'c';
+        lookingto = 'r';
+        break;
+      default: 
+        // Go back to center
+        neckServo.write(neckServoCenter);
+        lookingto = 'c';
+    }
+    
+    delay(1000);
+  }
 }
 
 void andarFrente(int speedA, int speedB)
@@ -87,4 +163,28 @@ void parar(int speedA, int speedB)
   digitalWrite(M2,LOW);
   analogWrite(E1, 0);   //PWM Speed Control
   analogWrite(E2, 0);   //PWM Speed Control
+}
+
+void thebestway()
+{
+  switch (shouldgo) {
+    case 'l':
+      andarEsquerda(0,0);
+      break;
+    case 'r':
+      andarDireita(0,0);
+      break;
+    case 'b':
+      andarRe(0,0);
+      delay(1000);
+      andarDireita(0,0);
+      delay(1500);
+      break;
+    default:
+      break;
+  }
+  
+  delay(500);
+  parar(0,0);
+  goon = true;
 }
